8cfa2cf8f799684bd605444e74ce71c5ea59b982
Lukas Gafner <62466873+Kasluk24@users.noreply.github.com>
2020-10-29 12:14:04 +0100

    App doesn't generate duplicates anymore
    Several changes to different functions

--- src/UsercodeGenerator.py
+++ src/UsercodeGenerator.py
@@ -8,6 +8,11 @@ import argparse
 import datetime
 import os
 
+# Pseudo Constants
+LETTER_COUNT = 9
+GROUP_SIZE = 3
+SEPARATOR_CHAR = '-'
+
 # Argument parser
 parser = argparse.ArgumentParser(description='Create random letter codes')
 parser.add_argument('--randseed', dest='randseed', action='store_const', const=0, default=1, help='Takes a random number for the seed and does not print it out.')
@@ -19,38 +24,56 @@ args = parser.parse_args()
 # Functions
 # Generate the codes
 def createCode(arg_seed, arg_count):
-    var_randnums = []
-    var_randabc = []
-    var_numcodes = []
-    var_codes = []
     var_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    
+    var_finalcodes = ['dummy'] # Lettercodes as String without separator (dummy will be deletet at the end
+
     random.seed(arg_seed)
     
-    for i in range(9 * arg_count):
-        var_randnums.append(random.randint(0, len(var_letters) - 1))
+    var_i = 0
+    while var_i < arg_count:
+        var_j = 0
+        while var_j < 1:
+            var_lettercode = "" # Single lettercode as a String
         
-    for k in range(len(var_randnums)):
-        var_randabc.append(var_letters[var_randnums[k]])
+            for k in range(LETTER_COUNT):            
+                var_lettercode = var_lettercode + var_letters[random.randint(0, len(var_letters) - 1)]     
+            
+            var_j += 1
+            
+            for l in range(len(var_finalcodes)):
+                if var_finalcodes[l] == var_lettercode:
+                    var_j -= 1
+                    break
+        
+        var_finalcodes.append(var_lettercode)
+        var_i = var_i + 1 
     
-    var_l = 0
-    for i in range(arg_count):
-        for m in range(9):
-            var_randabc.append(var_letters[random.randint(0, len(var_letters) - 1)])
-            var_codes.append()
+    del var_finalcodes[0]
+    
+    formatCodes(var_finalcodes, arg_count, arg_seed)
+    
+# Formats the codes with separators   
+def formatCodes(arg_finalcodes, arg_count, arg_seed):
+    var_formatcodes = [] # List with the formatted Codes as String ready for printing
+    
+    for i in range(len(arg_finalcodes)):
+        var_lettercode = arg_finalcodes[i]
+        var_formatcode = ''
         
-        var_randnums
-        var_singlecode = ''
+        for j in range(int(LETTER_COUNT / GROUP_SIZE)):
+            var_formatcode += var_lettercode[j * GROUP_SIZE:(j + 1) * GROUP_SIZE]
+            var_formatcode += SEPARATOR_CHAR
         
-        
-        
+        var_formatcode = var_formatcode[0:-1]
+        var_formatcodes.append(var_formatcode)
+    
     if args.export == 1:
-        pass
+        printCodes(var_formatcodes, arg_count, arg_seed)
     else:
-        pass
+        exportCodes(var_formatcodes, arg_count, arg_seed)
     
 # Print the codes
-def printCodes(arg_randabc, arg_count, arg_seed):
+def printCodes(arg_formatcodes, arg_count, arg_seed):
     if args.randseed == 1:
         print("Used seed: " + str(arg_seed))
     else:
@@ -60,15 +83,13 @@ def printCodes(arg_randabc, arg_count, arg_seed):
     print(datetime.datetime.now().strftime("%c"))
     print("****************************")
     
-    var_l = 0
-    for i in range(arg_count):
-        print(arg_randabc[var_l] + arg_randabc[var_l + 1] + arg_randabc[var_l + 2] + "-" + arg_randabc[var_l + 3] + arg_randabc[var_l + 4] + arg_randabc[var_l + 5] + "-" + arg_randabc[var_l + 6] + arg_randabc[var_l + 7] + arg_randabc[var_l + 8])
-        var_l = var_l + 9
-    
+    for i in range(len(arg_formatcodes)):
+        print(arg_formatcodes[i])
+            
     print("****************************")
 
 # Write codes to file
-def exportCodes(arg_randabc, arg_count, arg_seed):
+def exportCodes(arg_formatcodes, arg_count, arg_seed):
     print('Define export directory')
     var_exportpath = input()
     print('Define export filename')
@@ -88,16 +109,21 @@ def exportCodes(arg_randabc, arg_count, arg_seed):
     efile.write(datetime.datetime.now().strftime("%c") + "\n")
     efile.write("****************************" + "\n")
     
-    var_l = 0
-    for i in range(arg_count):
-        efile.write(arg_randabc[var_l] + arg_randabc[var_l + 1] + arg_randabc[var_l + 2] + "-" + arg_randabc[var_l + 3] + arg_randabc[var_l + 4] + arg_randabc[var_l + 5] + "-" + arg_randabc[var_l + 6] + arg_randabc[var_l + 7] + arg_randabc[var_l + 8] + "\n")
-        var_l = var_l + 9
+    for i in range(len(arg_formatcodes)):
+        efile.write(arg_formatcodes[i] + "\n")
     
     efile.write("****************************" + "\n")
 
 # Begin of the program
 print('Number of codes to generate')
 var_codes = int(input())
+if var_codes > 10000:
+    print("Warning! You want to generate a large number of codes. Depending on your hardware the process can take a long time.")
+    print("Do you want to continue? [yes / no]")
+    if input() != "yes":
+        print("Exit application")
+        exit()
+
 if args.randseed == 1:
     print('Define seed')
     var_seed = int(input())
